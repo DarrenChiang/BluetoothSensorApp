b4699376b6a1e524376484eda0dd1728aa6182d4
Darren Chiang <nerradchiang@gmail.com>
2024-07-24 22:25:20 -0500

    Set up arrays

--- app/src/main/java/com/aqst/bluetoothsensorapp/presentation/BluetoothUiState.kt
+++ app/src/main/java/com/aqst/bluetoothsensorapp/presentation/BluetoothUiState.kt
@@ -17,9 +17,20 @@ data class BluetoothUiState(
     val deviceName: String? = null,
     val errorMessage: String? = null,
     val lastCommand: String? = null,
+
+//    To be implemented in BluetoothViewModel.kt
+//    Raw Data -> SG Filter -> 10 Average -> 2 Delta -> 10 Average -> Take positive else previous -> Take lesser else previous -> Step 5 - Step 6
+//                             (Step 2)      (Step 3)   (Step 4)      (Step 5)                       (Step 6)                     (Chart Data)
+
     val rawData: List<DataPoint> = emptyList(),
     val sgfData: List<DataPoint> = emptyList(),
+    val step2Data: List<DataPoint> = emptyList(),
+    val step3Data: List<DataPoint> = emptyList(),
+    val step4Data: List<DataPoint> = emptyList(),
+    val step5Data: List<DataPoint> = emptyList(),
+    val step6Data: List<DataPoint> = emptyList(),
     val chartData: List<Entry> = emptyList(),
+
     val chart: LineChart? = null,
     val chartMaxQueue: List<Entry> = emptyList(),
     val chartMinQueue: List<Entry> = emptyList(),
